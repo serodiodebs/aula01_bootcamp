72636727c8df604d0e1600308ee6e5a68a4728dc
serodiodebs <32377390+serodiodebs@users.noreply.github.com>
2024-02-20 22:11:29 -0300

    adicionar arquivo exemplo 2

--- exemplo02.py
+++ exemplo02.py
@@ -1,1 +1,6 @@
-print("Oi! Iniciando já com o Git configurado! Arquivo de teste 2")
+# Ex.: 02 - Digite dois valores e print a soma
+
+val1 = int(input("Digite um valor: "))
+val2 = int(input("Digite o segundo valor: "))
+
+print(val1 + val2)
